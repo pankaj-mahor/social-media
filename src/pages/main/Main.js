36ed6fe651beda9c2564162e967389432998a171
pankajdev1221 <pankaj.dev1221@gmail.com>
2022-09-19 11:49:53 +0530

    Added like , unlike functionalltty and intefrace

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,48 +1,46 @@
-import React , { useState , useEffect }  from 'react'
-import { getDocs , collection } from 'firebase/firestore'
-import { db } from '../../config/firebase'
-import Post from './Post'
+import React, { useState, useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import Post from "./Post";
 
 const Main = () => {
-    const [isloaded, setLoaded] = useState(false)
+    const [isloaded, setLoaded] = useState(false);
 
-  const [postList , setPostList]  = useState([])
-  const postRef = collection(db, "posts");
+    const [postList, setPostList] = useState([]);
+    const postRef = collection(db, "posts");
 
+    const getPosts = async () => {
+        const data = await getDocs(postRef);
+        setPostList(
+            data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) || []
+        );
+        console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-  const getPosts = async () => {
-    const data = await getDocs(postRef);
-    setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) || [])
-    console.log(data.docs.map((doc) => ({...doc.data() , id: doc.id})))    
-  }
+    useEffect(() => {
+        getPosts();
 
-  
+        // let flag = true/
+        // console.log('effect start')
+        // if (!isloaded) {
+        //   getPosts()
+        // }
+        // return () => {
+        //   setLoaded(true)
+        //   console.log('effect clean')
 
-  useEffect(() => {
+        // }
+    }, []);
 
-      getPosts()
+    return (
+        <div className="homepage">
+            <div className="posts">
+                {postList?.map((post, index) => (
+                    <Post key={index} post={post} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-    // let flag = true/
-    // console.log('effect start')
-    // if (!isloaded) {
-    //   getPosts()
-    // }
-    // return () => {
-    //   setLoaded(true)
-    //   console.log('effect clean')
-
-    // }
-  }, [])
-  
-  
-  return (
-    <div className='homepage'>{
-      postList?.map((post , index) => (
-        <Post key={index} post={ post} />
-    ))
-    
-    }</div>
-  )
-}
-
-export default Main
+export default Main;
--- src/pages/main/Post.js
+++ src/pages/main/Post.js
@@ -1,55 +1,94 @@
-import React, { useState , useEffect } from 'react'
-import { auth, db } from '../../config/firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { addDoc ,collection , query, where  , getDocs} from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { addDoc, collection, query, where, getDocs  ,deleteDoc , doc} from "firebase/firestore";
 const Post = (props) => {
-    const [postliked , setPostLiked] = useState(false)
-    const [likedAmount , setlikedAmount] = useState(0)
-    const [user] = useAuthState(auth)
+    
+    const [likes, setLikes] = useState([]);
+    const [user] = useAuthState(auth);
 
     const { post } = props;
 
-     //firebase collection ref
-    const likesRef = collection(db , "likes")
+    //firebase collection ref
+    const likesRef = collection(db, "likes");
 
+    const likesDoc = query(likesRef, where("postId", "==", post.id));
 
-    const likesDoc = query(likesRef, where("postId", "==", post.id))
-    
     const getLikes = async () => {
-        const data = await getDocs(likesDoc)
-        setlikedAmount(data.docs.length)
-        // console.log(data.docs.map((doc) => ({...doc.data() , id: doc.id})))
-    }
+        const data = await getDocs(likesDoc);
+        // setLikek(data.docs.length)
+        setLikes(data.docs.map((doc) => ({ userId: doc.data().userId  , likeId: doc.id})));
+    };
 
     const addLike = async (data) => {
-        // console.log(data) 
-        await addDoc(likesRef , {userId: user?.uid , postId: post.id })
-        setPostLiked(true)
-    }
+        // console.log(data)
+
+        try {
+            const newDoc = await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+     
+            if (user) {
+                setLikes((prev) =>
+                    prev
+                        ? [...prev, { userId: user?.uid  , likeId : newDoc.id}]
+                        : [{ userId: user?.uid  ,  likeId : newDoc.id}]
+                );
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const removeLike = async (data) => {
+        // console.log(data)
+
+        try {
+            const likeTodeleteQuery = query(likesRef, where("postId", "==", post.id) , where("userId" ,"==" , user.uid));
+
+            const likeToDeleteData = await getDocs(likeTodeleteQuery)
+            const likeId = likeToDeleteData.docs[0].id;
+            const likeToBeDeleted = doc(db , "likes" , likeId)
+            await deleteDoc(likeToBeDeleted);
+           
+            if (user) {
+                setLikes(((prev) => prev && prev.filter((like) => like.likeId !== likeId)));
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const hasUserLiked = likes?.find((like) => like?.userId === user?.uid);
 
     useEffect(() => {
-      getLikes()
-    }, [])
-    
-  return (
-      <div className='post'>
-          <div className='title'>
-              
-        <h2>{post.title}</h2>     
-          </div>
-          <div className='body'>
-              
-        <p className='desc-post'>{ post.description}</p>
-          </div>
-          <div className='footer'>
-              <p>@{post.username}</p>
-              <button onClick={addLike} style={postliked ? {background: 'limegreen' } : {background:'cream'}}> &#128077;</button>
-                
-              {likedAmount > 0 ? (<p>Likes: {likedAmount} </p>) : null }
-              
-          </div>
-    </div>
-  )
-}
+        getLikes();
+    }, []);
 
-export default Post
+    return (
+        <div className="post">
+            <div className="title">
+                <h2>{post.title}</h2>
+            </div>
+            <div className="body">
+                <p className="desc-post">{post.description}</p>
+            </div>
+            <div className="footer">
+                <p>@{post.username}</p>
+
+                <button
+                    onClick={hasUserLiked ? removeLike : addLike}
+                    // style={
+                    //     postliked
+                    //         ? { background: "limegreen" }
+                    //         : { background: "cream" }
+                    // }
+                >
+                    {hasUserLiked ? <>&#128078;</> : <>&#128077;</>}
+                </button>
+
+                {likes && <p>Likes: {likes.length} </p>}
+            </div>
+        </div>
+    );
+};
+
+export default Post;
